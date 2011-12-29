97fd7e767d87add029ed5692f73e5a661d35ca35
Utku Demir <utdemir@gmail.com>
2011-12-29 22:26:03 +0200

    Handle command line arguments with argparse. Suggest opening settings dialog on exceptions.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import argparse
 
 import sip
 sip.setapi("QString", 2)
@@ -10,10 +11,29 @@ from PyQt4.QtGui import QApplication
 
 from mainui import MainUi
 
+parser = argparse.ArgumentParser(description="SimpleShortcuts")
+parser.add_argument("-s", "--settings", 
+                    help="Show the settings dialog",
+                    action="store_true")
+                    
+args = parser.parse_args()
+
 app = QApplication(sys.argv)
 rect = app.desktop().screenGeometry()
 
-g = MainUi()
+if(args.settings):
+    from settingsdialog import SettingsDialog
+    g = SettingsDialog()
+else:
+    try:
+        g = MainUi()
+    except Exception as err:
+        print("An exception has occured!")
+        print("Try opening the settings dialog with -s parameter and resaving the settings.",
+              "You may have to delete your settings file as last resort."
+            )
+        raise err
+    
 g.show()
 
 g.move(rect.center() - g.rect().center())
